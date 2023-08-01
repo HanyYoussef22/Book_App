16ec1a187a1ab6ec7446500c947ed980624d95e9
HanyYous-sef <109626022+HanyYous-sef@users.noreply.github.com>
2023-08-01 18:05:10 +0300

    Initial commit

---
+++ lib/Features/presentation/view/SplashView.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+import 'widgets/SplahViewBody.dart';
+
+class SplahView extends StatelessWidget {
+  const SplahView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SplahViewBody(),
+    );
+  }
+}
+ 
