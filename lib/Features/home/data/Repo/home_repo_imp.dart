f52f7d47b1fe711b0523c6bcb23b2c2fe45b4ea5
HanyYous-sef <109626022+HanyYous-sef@users.noreply.github.com>
2023-08-21 22:57:07 +0300

    implement featch book

--- lib/Features/home/data/Repo/home_repo_imp.dart
+++ lib/Features/home/data/Repo/home_repo_imp.dart
@@ -1,16 +1,35 @@
 import 'package:book_app/Features/home/data/model/ModelBook.dart';
 
 import 'package:book_app/core/errors/failures.dart';
+import 'package:book_app/core/utils/api_service.dart';
 
 import 'package:dartz/dartz.dart';
 
 import 'home_repo.dart';
 
 class HomeRepoImplement implements HomeRepo{
+   final ApiServise apiServise;
+
+  HomeRepoImplement(this.apiServise);
   @override
-  Future<Either<Failure, List<ModelBook>>> featchBestSellerBooks() {
-    // TODO: implement featchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<ModelBook>>> featchBestSellerBooks()async {
+    try
+    {
+      var response = await apiServise.get(endPoint: 'volumes?Filtering=free-ebooks&Sorting=newest &subject=&q=subject:general');
+      // return response.map((e)=>ModelBook.fromJson(e)).toList();
+      List<ModelBook> books=[];
+      for(var item in response['items']){
+        books.add(ModelBook.fromJson(item));
+      }
+      return right(books);
+    }
+    on Exception catch (e){
+
+      return left(ServerFailure());
+    }
+
+
+
   }
 
   @override
